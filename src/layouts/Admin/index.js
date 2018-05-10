859bdb8b8d21a9c17027e1d7d288cde210a7b565
LinhNdD <haixoay96@gmail.com>
2018-05-10 07:29:14 +0700

    fix

---
+++ src/layouts/Admin/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import Base from '../Base';
+
+
+
+export default class Admin extends Base{
+    renderContent(){
+        return(
+            <div>Hello</div>
+        )
+    }
+}
